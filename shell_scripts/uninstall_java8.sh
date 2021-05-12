719c16e44e0cdd0be05d1eac728e7c341f06131f
therealxbx <xbxlolhd@gmail.com>
2021-05-12 17:32:15 +0200

    Update uninstall_java8.sh

--- shell_scripts/uninstall_java8.sh
+++ shell_scripts/uninstall_java8.sh
@@ -3,11 +3,11 @@
 clear
 echo "Removing Java 8 Update Alternatives"
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/java/jdk1.8.0_211/bin/java" 1
+sudo update-alternatives --remove "/usr/bin/java" "java" "/usr/lib/java/jdk1.8.0_211/bin/java" 1
 
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/java/jdk1.8.0_211/bin/javac" 1
+sudo update-alternatives --remove "/usr/bin/javac" "javac" "/usr/lib/java/jdk1.8.0_211/bin/javac" 1
 
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/java/jdk1.8.0_211/bin/javaws" 1
+sudo update-alternatives --remove "/usr/bin/javaws" "javaws" "/usr/lib/java/jdk1.8.0_211/bin/javaws" 1
 
 clear
 echo "Removing Java 8 folder"
